eaba5f335bd03acae7afe45ddad6ec2bd92eaedb
mateocollar_ <collarmateo0610@gmail.com>
2025-05-20 21:30:10 -0300

    Actualizar scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
-const toggle = document.getElementById('menu-toggle');
-const nav = document.getElementById('nav-links');
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("menu-toggle");
+  const navLinks = document.getElementById("nav-links");
 
-toggle.addEventListener('click', () => {
-  nav.classList.toggle('show');
+  toggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
 });
